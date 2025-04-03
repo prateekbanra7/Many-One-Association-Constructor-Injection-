b0ec6479ffaf778d925cd80d7a744163a68e5349
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-04-03 22:32:16 +0530

    Add files via upload

---
+++ Branch.java
@@ -0,0 +1,25 @@
+package in.abc.entities;
+
+//Dependent object
+public class Branch {
+	private String bid;
+	private String bloc;
+	
+	//Constructor injection
+	public Branch(String bid, String bloc) {
+		
+		this.bid = bid;
+		this.bloc = bloc;
+	}
+
+	//Getters to get the value
+	public String getBid() {
+		return bid;
+	}
+
+	public String getBloc() {
+		return bloc;
+	}
+	
+	
+}
---
+++ TestApp.java
@@ -0,0 +1,30 @@
+package in.abc.test;
+
+import in.abc.entities.Branch;
+import in.abc.entities.Student;
+
+public class TestApp {
+
+	//Driving code
+	public static void main(String[] args) {
+		
+		//Creating a dependent object
+		Branch branch = new Branch("IN01","VJYNGR");
+		
+		//Creating a target object
+		Student s1 = new Student(10,"sachin",49,branch);
+		Student s2 = new Student(7,"dhoni",41,branch);
+		Student s3 = new Student(18, "kohli", 35, branch);
+		
+		s1.getStudentDetails();
+		System.out.println();
+		
+		s2.getStudentDetails();
+		System.out.println();
+		
+		s3.getStudentDetails();
+		
+
+	}
+
+}
